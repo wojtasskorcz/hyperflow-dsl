c4fc8b30a9044ee51d80041151fa5a7f23ab9a9c
wojtasskorcz <wojtasskorcz@gmail.com>
2015-01-04 17:13:20 +0100

    Tested comment parsing

--- src/test/scala/hdsl/test/parser/RegexUnitTest.scala
+++ src/test/scala/hdsl/test/parser/RegexUnitTest.scala
@@ -4,6 +4,8 @@ import hdsl.test.UnitSpec
 
 import scala.util.parsing.combinator.JavaTokenParsers
 
+import org.junit.Assert._
+
 class RegexUnitTest extends UnitSpec {
 
   test("That curly brackets content is parsed correctly") {
@@ -27,6 +29,23 @@ class RegexUnitTest extends UnitSpec {
     for (example <- examples) {
       assert(parser.parseAll(parser.bracketsParser, example).successful)
     }
+  }
+
+  test("That comments end on newline") {
+    val parser = new JavaTokenParsers {
+      def mainParser: Parser[List[String]] = rep(comment | ident)
+      def comment: Parser[String] = "//.*".r
+    }
+    val commentThenWord =
+      """
+        |firstWord
+        |// first comment
+        |secondWord
+        |// second comment
+      """.stripMargin
+    val parsingResult = parser.parseAll(parser.mainParser, commentThenWord)
+    assert(parsingResult.successful)
+    assertEquals(List("firstWord", "// first comment", "secondWord", "// second comment"), parsingResult.get)
   }
 
 }
