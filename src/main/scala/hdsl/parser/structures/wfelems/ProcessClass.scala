7deb23ea72c436626f9b170a61d56f3640a41ece
wojtasskorcz <wojtasskorcz@gmail.com>
2015-01-22 13:25:14 +0100

    Changed helper functions of setProperty() and resolvedPropertiesMap() to inner functions

--- src/main/scala/hdsl/parser/structures/wfelems/ProcessClass.scala
+++ src/main/scala/hdsl/parser/structures/wfelems/ProcessClass.scala
@@ -11,37 +11,39 @@ case class ProcessClass(name: String, args: List[Arg], returnType: String, invoc
   val properties = mutable.Map.empty[String, Any]
 
   def setProperty(path: List[String], rhs: Atomic) = {
+
+    def recursivelySetProperty(path: List[String], rhs: Atomic, targetMap: MutableMap[String, Any]): Unit = {
+      path match {
+        case List(lastProperty) => targetMap.put(lastProperty, rhs)
+        case longerList => {
+          val innerMap = targetMap.get(longerList(0)) match {
+            case Some(innerMap: MutableMap[_, _]) => innerMap.asInstanceOf[MutableMap[String, Any]]
+            case _ => {
+              val innerMap = mutable.Map.empty[String, Any]
+              targetMap.put(path(0), innerMap)
+              innerMap
+            }
+          }
+          recursivelySetProperty(path.drop(1), rhs, innerMap)
+        }
+      }
+    }
+
     recursivelySetProperty(path, rhs, properties)
   }
 
-  private def recursivelySetProperty(path: List[String], rhs: Atomic, targetMap: MutableMap[String, Any]): Unit = {
-    path match {
-      case List(lastProperty) => targetMap.put(lastProperty, rhs)
-      case longerList => {
-        val innerMap = targetMap.get(longerList(0)) match {
-          case Some(innerMap: MutableMap[_, _]) => innerMap.asInstanceOf[MutableMap[String, Any]]
-          case _ => {
-            val innerMap = mutable.Map.empty[String, Any]
-            targetMap.put(path(0), innerMap)
-            innerMap
-          }
-        }
-        recursivelySetProperty(path.drop(1), rhs, innerMap)
-      }
-    }
-  }
-
   def resolvedPropertiesMap(/* TODO classes, instances, etc. needed for resolution */): MutableMap[String, Any] = {
-    recursivelyResolveProperties(properties)
-  }
 
-  private def recursivelyResolveProperties(map: MutableMap[String, Any]): MutableMap[String, Any] = {
-    val outMap = map.map({
-      case (s, atomic: Atomic) => (s, atomic.evaluate)
-      case (s, innerMap: MutableMap[_, _]) => (s, recursivelyResolveProperties(innerMap.asInstanceOf[MutableMap[String, Any]]))
-      case (s, any) => throw new RuntimeException(s"Unexpected $any")
-    })
-    outMap
+    def recursivelyResolveProperties(map: MutableMap[String, Any]): MutableMap[String, Any] = {
+      val outMap = map.map({
+        case (s, atomic: Atomic) => (s, atomic.evaluate)
+        case (s, innerMap: MutableMap[_, _]) => (s, recursivelyResolveProperties(innerMap.asInstanceOf[MutableMap[String, Any]]))
+        case (s, any) => throw new RuntimeException(s"Unexpected $any")
+      })
+      outMap
+    }
+
+    recursivelyResolveProperties(properties)
   }
 
 }
