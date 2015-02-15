de44b9770e413fb9065894b84f358aa8b159f228
wojtasskorcz <wojtasskorcz@gmail.com>
2015-02-15 12:12:06 +0100

    Parsing tupled indexed arrays of signals/processes

--- src/main/scala/hdsl/parser/HdslParser.scala
+++ src/main/scala/hdsl/parser/HdslParser.scala
@@ -1,36 +1,36 @@
 package hdsl.parser
 
 import hdsl.parser.structures._
-import hdsl.parser.structures.rhs.{Rhs, SignalInstantiation, ProcessInstantiation, Expr}
+import hdsl.parser.structures.rhs.{Expr, ProcessInstantiation, Rhs, SignalInstantiation}
 import hdsl.parser.structures.wfelems._
 
 import scala.util.parsing.combinator.JavaTokenParsers
 
 object HdslParser extends JavaTokenParsers {
-  
-  def workflow: Parser[List[WfElem]] = rep(workflowElem | forLoop) ^^ {case elems => elems filterNot(elem => elem == null)}
+
+  def workflow: Parser[List[WfElem]] = rep(workflowElem | forLoop) ^^ { case elems => elems filterNot (elem => elem == null)}
 
   def workflowElem: Parser[WfElem] = signalClass | processClass | assignment | composition | comment
-  
+
   def signalClass: Parser[SignalClass] = "signal" ~> ident ~ ("(" ~> signalClassArgs <~ ")") ^^ {
     case name ~ args => SignalClass(name, args)
   }
 
   def signalClassArgs: Parser[List[Arg]] = repsep(signalClassArg, ",") ^^ (List() ++ _)
 
-  def signalClassArg: Parser[Arg] = ident ~ ":" ~ signalClassArgType ^^ {case name ~ ":" ~ argType => Arg(name, argType, Nil)}
+  def signalClassArg: Parser[Arg] = ident ~ ":" ~ signalClassArgType ^^ { case name ~ ":" ~ argType => Arg(name, argType, Nil)}
 
   def signalClassArgType: Parser[String] = "String"
 
   def processClass: Parser[ProcessClass] =
     "process" ~> ident ~ ("(" ~> processClassArgs <~ ")") ~ opt(":" ~> ident) ~ ("{" ~> processBody <~ "}") ^^ {
-    case name ~ args ~ Some(returnType) ~ ((settings, invocation)) => {
-      val processClass = ProcessClass(name, args, returnType, invocation)
-      settings.foreach(assignment => processClass.setProperty(assignment.lhs.parts, assignment.rhs.asInstanceOf[Expr]))
-      processClass
+      case name ~ args ~ Some(returnType) ~ ((settings, invocation)) => {
+        val processClass = ProcessClass(name, args, returnType, invocation)
+        settings.foreach(assignment => processClass.setProperty(assignment.lhs.parts, assignment.rhs.asInstanceOf[Expr]))
+        processClass
+      }
+      case name ~ args ~ None ~ ((settings, invocation)) => ProcessClass(name, args, "Signal", invocation)
     }
-    case name ~ args ~ None ~ ((settings, invocation)) => ProcessClass(name, args, "Signal", invocation)
-  }
 
   def processClassArgs: Parser[List[Arg]] = repsep(processClassArg, ",")
 
@@ -42,7 +42,7 @@ object HdslParser extends JavaTokenParsers {
 
   def modifier: Parser[String] = "sticky"
 
-  def argWithImplicitType: Parser[Arg] = ident ^^ {case name => Arg(name, "Signal", Nil)}
+  def argWithImplicitType: Parser[Arg] = ident ^^ { case name => Arg(name, "Signal", Nil)}
 
   def processBody: Parser[(List[WfElemAssignment], FunctionInvocation)] = rep(processSettings) ~ functionInvocation ^^ {
     case settings ~ invocation => (settings, invocation)
@@ -57,13 +57,13 @@ object HdslParser extends JavaTokenParsers {
   }
 
   def assignment: Parser[Assignment] = "var" ~> ident ~ "=" ~ expr ^^ {
-        case varName ~ "=" ~ expr => VarAssignment(varName, expr)
-      } |
-      lhs ~ "=" ~ rhs ^^ {
-          case lhs ~ "=" ~ rhs => WfElemAssignment(lhs, rhs)
+    case varName ~ "=" ~ expr => VarAssignment(varName, expr)
+  } |
+    lhs ~ "=" ~ rhs ^^ {
+      case lhs ~ "=" ~ rhs => WfElemAssignment(lhs, rhs)
     }
 
-  def lhs: Parser[DotNotationAccessor] = rep1sep(ident, ".") ^^ {case parts => DotNotationAccessor(parts)}
+  def lhs: Parser[DotNotationAccessor] = rep1sep(ident, ".") ^^ { case parts => DotNotationAccessor(parts)}
 
   def rhs: Parser[Rhs] = signalInstantiation | processInstantiation | expr
 
@@ -77,11 +77,11 @@ object HdslParser extends JavaTokenParsers {
     case name ~ None => ProcessInstantiation(name, null)
   }
 
-  def expr: Parser[Expr] = "true" ^^ {case _ => Expr(true)} |
-    "false" ^^ {case _ => Expr(false)} |
-    stringLiteral ^^ {case str => Expr(str)} |
-    floatingPointNumber ^^ {case num => Expr(num.toDouble)} |
-    ident ^^ {case varName => Expr(varName)}
+  def expr: Parser[Expr] = "true" ^^ { case _ => Expr(true)} |
+    "false" ^^ { case _ => Expr(false)} |
+    stringLiteral ^^ { case str => Expr(str)} |
+    floatingPointNumber ^^ { case num => Expr(num.toDouble)} |
+    ident ^^ { case varName => Expr(varName)}
 
   def arrayAccessor: Parser[Expr] = "[" ~> expr <~ "]"
 
@@ -92,13 +92,15 @@ object HdslParser extends JavaTokenParsers {
   def compositionElem: Parser[CompositionElem] = ident ~ opt(arrayAccessor) ~ opt(":" ~> lhs) ^^ {
     case name ~ idx ~ additional => CompositionElem(List(name), List(idx.orNull), additional.orNull)
   } |
-    "(" ~> rep1sep(ident, ",") <~ ")" ^^ {case names => CompositionElem(names, List.fill(names.length)(null), null)}
+    "(" ~> rep1sep(ident ~ opt(arrayAccessor), ",") <~ ")" ^^ {
+      case tupledElems => CompositionElem(tupledElems.map(_._1), tupledElems.map(_._2.orNull), null)
+    }
 
   def forLoop: Parser[ForLoop] = "for" ~> "(" ~> ident ~ opt("," ~> ident) ~ "<-" ~ ident ~ (")" ~> "{" ~> rep(workflowElem) <~ "}") ^^ {
     case loopVar ~ Some(loopIdx) ~ "<-" ~ array ~ wfElems => ForLoop(loopVar, loopIdx, array, wfElems)
     case loopVar ~ None ~ "<-" ~ array ~ wfElems => ForLoop(loopVar, null, array, wfElems)
   }
-  
-  def comment: Parser[WfElem] = "//.*".r ^^ {case _ => null}
+
+  def comment: Parser[WfElem] = "//.*".r ^^ { case _ => null}
 
 }
--- src/main/scala/hdsl/parser/structures/CompositionElem.scala
+++ src/main/scala/hdsl/parser/structures/CompositionElem.scala
@@ -4,6 +4,17 @@ import hdsl.MutableMap
 import hdsl.compiler.structures.{ProcessInstance, Wf}
 import hdsl.parser.structures.rhs.Expr
 
+/**
+ * Represents a parsed composition element, which may have one of the forms (list not exhaustive):
+ * - 'p' name of an instantiated explicit process or a name of an explcitly or implicitly instantiated signal
+ * - 'PartitionData' name of a process class which will be implicitly instantiated
+ * - '(xml, config)' tuple containing combinations of the above (all tuple elements have to be either signals or
+ * processes)
+ * - 'stationsArr[idx]' element of array of signals or processes identified by an index
+ * - 'graphArr[idx]:stationsArr[idx].count' same as above but with a count signal from another array of processes
+ * - '(dataPartsArr[idx], config)' tuple of indexed signals or processes; each element of `nameIndices` list
+ * corresponds to each element of `names`
+ */
 case class CompositionElem(names: List[String], nameIndices: List[Expr], additional: DotNotationAccessor) {
 
   def isSignalElem() = {
